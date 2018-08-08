61fdccadedac212fead6be93edc7f5445542916e
Alexander-Tengborg <Landervich@Gmail.com>
2018-08-09 01:04:18 +0200

    Initial Commit

---
+++ code/win32_handmade.cpp
@@ -0,0 +1,17 @@
+/*
+ * @Author: Alexander Tengborg 
+ * @Date: 2018-08-08 22:06:52 
+ * @Last Modified by: Alexander Tengborg
+ * @Last Modified time: 2018-08-08 22:09:29
+ */
+
+#include <windows.h>
+
+int CALLBACK
+WinMain(HINSTANCE hInstance,
+        HINSTANCE hPrevInstance,
+        LPSTR     lpCmdLine,
+        int       nCmdShow)
+{
+    return (0);
+}
---
+++ misc/vscode.bat
@@ -0,0 +1,2 @@
+@echo off
+code "w:\handmade"
